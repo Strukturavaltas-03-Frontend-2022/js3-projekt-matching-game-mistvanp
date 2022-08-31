d6aabb95b3452d9a98e946b094e1771a8c7f4574
Istvan888 <marai.istvanp@gmail.com>
2022-08-31 17:28:19 +0200

    half done

---
+++ assets/game.js
@@ -0,0 +1,9 @@
+const section = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 6;
+
+playerLivesCount.textContent = playerLives
+
+const getData = () => [
+    
+]
